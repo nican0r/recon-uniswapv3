4c8dc1b0a45c332664b90535e1a728e9aa73645c
nelson-pereira8 <94120714+nelson-pereira8@users.noreply.github.com>
2024-02-20 16:26:34 -0300

    added recon files

---
+++ test/recon/Properties.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.6;
+
+import {Asserts} from '@chimera/Asserts.sol';
+import {Setup} from './Setup.sol';
+
+abstract contract Properties is Setup, Asserts {}
